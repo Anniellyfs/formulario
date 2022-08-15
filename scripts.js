5108f803dc7d4dbba80078eb99b3414eb2e4c3c2
Anniellyfs <annyfs2015@gmail.com>
2022-08-15 11:23:29 -0300

    Final commit

--- scripts.js
+++ scripts.js
@@ -6,7 +6,10 @@ const passwordConfirmation = document.getElementById("password-confirmation")
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
+
+    checkInputs();
 });
+
 
 function checkInputs(){
     const usernameValue = username.value;
@@ -16,6 +19,42 @@ function checkInputs(){
 
     if(usernameValue == ""){
         setErrorFor(username, "O nome de usuário é obrigatório.");
+    }else{
+        setSuccessFor(username);
+    }
+
+    if(emailValue == ""){
+        setErrorFor(email, "O email é obrigatório.");
+    }else if(!checkEmail(emailValue)){
+        setErrorFor(email, "Por favor, insira um email válido.")
+    }else{
+        setSuccessFor(email);
+    }
+
+    if(passwordValue == ""){
+        setErrorFor(password, "O senha é obrigatório.");
+    }else if(passwordValue.length < 7){
+        setErrorFor(password, "A senha tem que ter no mínimo 7 caracteres.")
+    }else{
+        setSuccessFor(password);
+    }
+
+    if(passwordConfirmationValue == ""){
+        setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatório.");
+    }else if(passwordConfirmationValue != passwordValue){
+        setErrorFor(passwordConfirmation, "As senhas não conferem.")
+    }else{
+        setSuccessFor(passwordConfirmation);
+    }
+
+    const formControls = form.querySelectorAll(".form-control");
+
+    const formIsValid = [...formControls].every((formControl) =>{
+        return(formControl.className == "form-control success");
+    });
+
+    if(formIsValid){
+        console.log("O formulário está 100% válido!")
     }
 }
 
@@ -24,13 +63,20 @@ function setErrorFor(input, message){
     const small = formControl.querySelector('small')
 
     //add a mensagem de erro
-
+    small.innerText = message;
     //ass a classe de erro
+    formControl.className = "form-control error"
 }
 
 function setSuccessFor(input){
     const formControl = input.parentElement;
 
     //add a classe de success
-    
+    formControl.className = "form-control success"
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
 }
